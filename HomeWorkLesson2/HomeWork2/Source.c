07075bd45d9992b5082cc8d718df24efadbb33cc
kanadeiar <kanadeiar@gmail.com>
2020-01-17 00:48:01 +0300

    init new homeworks of lesson 2

---
+++ HomeWorkLesson2/HomeWork2/Source.c
@@ -0,0 +1,21 @@
+#include <windows.h>
+#include <stdio.h>
+
+
+#include "Header.h"
+
+int main(int argc, char * args[])
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	PrintHeader();
+	
+
+	PrintFooter();
+	return 0;
+}
+
+
+
+
+
