324e1dc5cbbcac119ca8801952821b2e7626886d
kanadeiar <kanadeiar@gmail.com>
2020-01-27 04:54:12 +0300

    final last task

--- HomeWorkLesson4/HomeWork4/TasksLesson4.c
+++ HomeWorkLesson4/HomeWork4/TasksLesson4.c
@@ -63,8 +63,52 @@ void task1_counter()
 	getchar();
 }
 
-#define MSIZE 10
-
+#define MSIZE 16
+void lcs(const char * str1, const char * str2, int arr_index[MSIZE][MSIZE], char arr_types[MSIZE][MSIZE], int m, int n);
+void print_lcs(const char * str1, char arr_types[MSIZE][MSIZE], int n, int m);
+void task2_lcs()
+{
+	printf("\n**************************************************************************************\n");
+	printf("2. Решить задачу о нахождении длины максимальной общей последовательности с помощью матрицы.\n");
+	/////////////////////////////////////////////////////////////
+	printf("Введите первую строку:");
+	char str1[MSIZE];
+	scanf(" %s", &str1);
+	getchar();
+	printf("Введите вторую строку:");
+	char str2[MSIZE];
+	scanf(" %s", &str2);
+	//getchar();
+	//char str1[MSIZE] = "geekbrains";
+	//char str2[MSIZE] = "geekminds";
+	int arr_index[MSIZE][MSIZE];
+	char arr_types[MSIZE][MSIZE];
+	int n = strlen(str1);
+	int m = strlen(str2);
+	lcs(str1, str2, arr_index, arr_types, n, m);
+	
+	puts("Наибольшая общая последовательность:");
+	print_lcs(str1, arr_types, n, m);
+	puts("\nМатрица:");
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <=m; j++)
+			printf("%2d ", arr_index[i][j]);
+		printf("\n");
+	}
+	
+	////////////////////////////////////////////////////////////
+	getchar();
+}
+/**
+ * \brief функция нахождения подпоследовательности в подподследовательности
+ * \param str1 первая подподпоследовательность
+ * \param str2 вторая подподпоследовательность
+ * \param arr_index массив с матрицей
+ * \param arr_types массив с типами
+ * \param m длина первой
+ * \param n длина второй
+ */
 void lcs(const char * str1, const char * str2, int arr_index[MSIZE][MSIZE], char arr_types[MSIZE][MSIZE], int m, int n)
 {
 	if (n == 0 || m == 0)
@@ -94,7 +138,13 @@ void lcs(const char * str1, const char * str2, int arr_index[MSIZE][MSIZE], char
 			}
 		}
 }
-
+/**
+ * \brief вывод на экран результата нахождения подподпоследовательнсоти
+ * \param str1 первая подпоследовательность
+ * \param arr_types массив типов
+ * \param n длинна
+ * \param m длинна
+ */
 void print_lcs(const char * str1, char arr_types[MSIZE][MSIZE], int n, int m)
 {
 	if (n == 0 || m == 0)
@@ -110,32 +160,42 @@ void print_lcs(const char * str1, char arr_types[MSIZE][MSIZE], int n, int m)
 		print_lcs(str1, arr_types, n, m - 1);
 }
 
-void task2_lcs()
+#define MOVES_SIZE 8
+//ходы коня
+int possible_moves[MOVES_SIZE][2] = {
+	{-2, -1}, {-1, -2}, {1, -2}, {2, -1},
+	{2, 1}, {1, 2}, {-1, 2}, {-2, 1},
+};
+#define BOARD_SIZE 5
+const int moves_max = BOARD_SIZE * BOARD_SIZE - 1;
+//определение возможного хода
+int move_possible(int x, int y, int board[BOARD_SIZE][BOARD_SIZE])
 {
-	printf("\n**************************************************************************************\n");
-	printf("2. Решить задачу о нахождении длины максимальной общей последовательности с помощью матрицы.\n");
-	/////////////////////////////////////////////////////////////
-	printf("Введите первую строку:");
-	char str1[MSIZE];
-	scanf(" %s", &str1);
-	getchar();
-	printf("Введите вторую строку:");
-	char str2[MSIZE];
-	scanf(" %s", &str2);
-	getchar();
-	//char x[MSIZE] = "geekbra";
-	//char y[MSIZE] = "geekfua";
-	int arr_index[MSIZE][MSIZE];
-	char arr_types[MSIZE][MSIZE];
-	int n = strlen(str1);
-	int m = strlen(str2);
-	lcs(str1, str2, arr_index, arr_types, n, m);
-	
-	puts("Наибольшая общая последовательность:");
-	print_lcs(str1, arr_types, n, m);
-	
-	////////////////////////////////////////////////////////////
-	getchar();
+	return x >= 0 && y >=0 && x < BOARD_SIZE && y < BOARD_SIZE && board[x][y] == 0;
+}
+/**
+ * \brief поиск ходов коня по полю
+ * \param curr_x стартовая позиция
+ * \param curr_y стартовая позиция
+ * \param move_num текущее число - ход коня
+ * \param board доска для коня
+ * \param max_moves максимальное число ходов коня
+ * \return 
+ */
+int find_next(int curr_x, int curr_y, int move_num, int board[BOARD_SIZE][BOARD_SIZE], int max_moves)
+{
+	board[curr_x][curr_y] = move_num;
+	if (move_num > max_moves)
+		return 1;
+	for (int i = 0; i < MOVES_SIZE; i++)
+	{ //поиск места, установка лошади и дальнейший рекурсивный поиск
+		int next_x = curr_x + possible_moves[i][0];
+		int next_y = curr_y + possible_moves[i][1];
+		if (move_possible(next_x, next_y, board) && find_next(next_x, next_y, move_num + 1, board, max_moves))
+			return 1;
+	}
+	board[curr_x][curr_y] = 0; //не удалось найти слещее место для лошади
+	return 0;
 }
 
 void task3_horse()
@@ -143,7 +203,20 @@ void task3_horse()
 	printf("\n**************************************************************************************\n");
 	printf("3. Требуется обойти конем шахматную доску размером NxM, пройдя через все поля доски по одному разу. Здесь алгоритм решения такой же, как в задаче о 8 ферзях. Разница только в проверке положения коня.\n");
 	/////////////////////////////////////////////////////////////
-
+	int start_x = 2, start_y = 2;
+	int board[BOARD_SIZE][BOARD_SIZE] = {0};
+	if (find_next(start_x, start_y, 1, board, moves_max))
+	{
+		puts("Найденное решение:");
+		for (int i=0; i<BOARD_SIZE; i++)
+		{
+			for (int j=0; j<BOARD_SIZE; j++)
+				printf("%2d ", board[i][j]);
+			printf("\n");
+		}
+	}
+	else
+		puts("Решения нет!");
 	////////////////////////////////////////////////////////////
 	getchar();
 }
