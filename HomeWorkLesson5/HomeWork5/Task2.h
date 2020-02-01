1c444aa4a718d0d9b3ddc164b9c04a5a6441c279
kanadeiar <kanadeiar@gmail.com>
2020-02-01 10:07:21 +0300

    task 2 complete

--- HomeWorkLesson5/HomeWork5/Task2.h
+++ HomeWorkLesson5/HomeWork5/Task2.h
@@ -1,2 +1,66 @@
 #pragma once
+#include <stdio.h>
+#include <malloc.h>
+#define T char
+
+struct TNode
+{
+	T value;
+	struct TNode * next;
+};
+typedef struct TNode Node;
+
+struct Stack
+{
+	Node * head;
+	int size;
+	int maxSize;
+};
+struct Stack Stack;
+
+void Push(T value)
+{
+	if (Stack.size >= Stack.maxSize)
+	{
+		printf("Переполнение стека!");
+		getchar();
+		exit(1);
+	}
+	Node * temp = (Node*) malloc(sizeof(Node));
+	if (temp == NULL)
+	{
+		printf("Ошибка выделения памяти!");
+		getchar();
+		exit(1);
+	}
+	temp->value = value;
+	temp->next = Stack.head;
+	Stack.head = temp;
+	Stack.size++;
+}
+
+T Pop()
+{
+	if (Stack.size == 0)
+	{
+		printf("Стек пуст!");
+		return -1;
+	}
+	Node * temp = Stack.head;
+	T value = Stack.head->value;
+	Stack.head = Stack.head->next;
+	free(temp);
+	Stack.size--;
+	return value;
+}
+
+void PrintStack()
+{
+	Node * curr = Stack.head;
+	while (curr != NULL)
+	{
+		printf(" %c", curr->value);
+		curr = curr->next;
+	}
+}
 
